a0d8481eb3af02a31561d3f5b17bb93381eba832
lakrestofer <lakrestofer@gmail.com>
2025-06-19 11:19:52 +0200

    update flake configuration

--- flake.nix
+++ flake.nix
@@ -11,7 +11,14 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      ...
+    }@inputs:
     let
       forEachSystem = nixpkgs.lib.genAttrs (import systems);
     in
@@ -21,27 +28,36 @@
         devenv-test = self.devShells.${system}.default.config.test;
       });
 
-      devShells = forEachSystem
-        (system:
-          let
-            pkgs = nixpkgs.legacyPackages.${system};
-          in
-          {
-            default = devenv.lib.mkShell {
-              inherit inputs pkgs;
-              modules = [
-                {
-                  # https://devenv.sh/reference/options/
-                  packages = [ pkgs.hello ];
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                packages = [ ];
 
-                  enterShell = ''
-                    hello
-                  '';
+                languages.rust = {
+                  enable = true;
+                  channel = "nixpkgs";
+                  components = [
+                    "rustc"
+                    "cargo"
+                    "clippy"
+                    "rustfmt"
+                    "rust-analyzer"
+                  ];
+                };
 
-                  processes.hello.exec = "hello";
-                }
-              ];
-            };
-          });
+                # enterShell = '''';
+                # processes.hello.exec = "hello";
+              }
+            ];
+          };
+        }
+      );
     };
 }
