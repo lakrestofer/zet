153c72995865d8e8c13dd17a21ec477bbb131693
lakrestofer <lakrestofer@gmail.com>
2025-12-25 19:34:04 +0100

    break out node to its own file

---
+++ src/core/types/node.rs
@@ -0,0 +1,53 @@
+use crate::core::{parser::ast_nodes::NodeKind, types::document::DocumentId};
+use rusqlite::{
+    ToSql,
+    types::{FromSql, FromSqlError, ToSqlOutput},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Node {
+    pub id: i64,
+    pub document_id: DocumentId,
+    pub node_type: NodeKind,
+    pub range_start: usize,
+    pub range_end: usize,
+    pub data: serde_json::Value,
+}
+
+impl Node {
+    pub fn new(
+        id: i64,
+        document_id: DocumentId,
+        node_type: NodeKind,
+        range_start: usize,
+        range_end: usize,
+        data: serde_json::Value,
+    ) -> Self {
+        Self {
+            id,
+            document_id,
+            node_type,
+            range_start,
+            range_end,
+            data,
+        }
+    }
+}
+
+impl FromSql for NodeKind {
+    fn column_result(value: rusqlite::types::ValueRef<'_>) -> rusqlite::types::FromSqlResult<Self> {
+        let str_value = value.as_str()?;
+        let value = serde_json::from_str(str_value).map_err(|_| FromSqlError::InvalidType)?;
+        Ok(value)
+    }
+}
+
+impl ToSql for NodeKind {
+    fn to_sql(&self) -> rusqlite::Result<ToSqlOutput<'_>> {
+        match serde_json::to_string(self) {
+            Ok(str) => Ok(str.into()),
+            Err(_) => panic!("Could not serialize NodeKind as string"),
+        }
+    }
+}
